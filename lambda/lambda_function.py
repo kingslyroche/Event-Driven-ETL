745725c4c47edab92c1b22075bdddd8a82a00d1f
Kingsly Roche <kingslyroche@gmail.com>
2021-03-28 20:45:51 +0530

    update insert logic

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -20,15 +20,45 @@ def notify(text):
         sns.publish(TopicArn = snsARN, Message = text)
     except Exception as e:
         print("Not able to send notification due to {}".format(e))
-        exit(1)
+        sys.exit(1)
 
 def fetch_datasets(url):
     try:
         df=pd.read_csv(url)
         return df
     except Exception as e:
-        print("Failed fetching datasets - {}".format(e))
-        exit(1)
+        notify("Failed fetching datasets - {}".format(e))
+        sys.exit(1)
+
+param_dic = {
+    "host"      : rdsEndpoint,
+    "database"  : rdsDatabaseName,
+    "user"      : rdsUser,
+    "password"  : rdsPassword
+}
+
+def connect(params_dic):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        conn = psycopg2.connect(**params_dic)
+    except (Exception, psycopg2.DatabaseError) as error:
+        notify("Failed connecting to database- {}".format(e))
+        sys.exit(1) 
+    return conn
+
+def single_insert(conn, insert_req):
+    """ Execute a single INSERT request """
+    cursor = conn.cursor()
+    try:
+        cursor.execute(insert_req)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        notify("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    cursor.close()
 
 def lambda_handler(event, context):
 
@@ -36,8 +66,16 @@ def lambda_handler(event, context):
 
     jh_df=fetch_datasets(johnHopkinsURL)
 
-    final_df=transform(nyt_df,jh_df)
+    dataframe=transform(nyt_df,jh_df)
 
-    connect_string = "postgresql://{}:{}@{}:{}/{}" .format(rdsUser,rdsPassword,rdsEndpoint,rdsPort,rdsDatabaseName)
+    # Connecting to the database
+    conn = connect(param_dic)
 
-    final_df.to_sql("covid_case",con=connect_string,if_exists='replace')
+    # Inserting each row
+    for index, row in dataframe.iterrows():
+        query = """
+        INSERT into covid_case(date, cases, deaths, Recovered) values('%s',%s,%s,%s);
+        """ % (row['date'], row['cases'], row['deaths'],int(row['Recovered']))
+        single_insert(conn, query)
+    # Close the connection
+    conn.close()
--- lambda/transformation.py
+++ lambda/transformation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def transform(nyt_df,jh_df):
-    nyt_df["date"]=pd.to_datetime(nyt_df["date"])
-    jh_df["Date"]=pd.to_datetime(jh_df["Date"])
+    nyt_df["date"]=pd.to_datetime(nyt_df["date"],infer_datetime_format=True).dt.date
+    jh_df["Date"]=pd.to_datetime(jh_df["Date"],infer_datetime_format=True).dt.date
     jh_df=jh_df[jh_df["Country/Region"]=="US"]
     merge_df=pd.merge(nyt_df,jh_df,how="inner",left_on='date', right_on='Date')
     final_df=merge_df[["date","cases","deaths","Recovered"]]
